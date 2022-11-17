706b38b50a845ef10498079898e21cc389c9a0f4
Ideal <ideal.buzoku.21@gmail.com>
2022-11-17 04:45:17 +0000

    Implement final function that output score to console.
    Bugfix problem where .toLowerCase seemed not to work. Use chrome debugger to great effect - great accomplishment.
    Breaking good commit message character, really happy with this one. Had a bug but fixed it using the debugger after first trying
    to use console.log() (print) statments. It works well and took me only 1 hour and 15 mins with me learning lots about Javascript.
    Hooray! Hopefully did better at committing too!

--- main.js
+++ main.js
@@ -1,44 +1,73 @@
 function getComputerChoice() {
-    randomNumber = Math.floor(Math.random() * 3);
+    let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber == 0) {
-        return "Rock";
+        return "rock";
     }
     else if (randomNumber == 1) {
-        return "Paper";
+        return "paper";
     }
     else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
     playerSelection.toLowerCase();
-    computerSelection.toLowerCase();
      
-    if (playerSelection == "rock" && computerSelection == "scissors") {
+    if (playerSelection === "rock" && computerSelection === "scissors") {
         return "You win! Rock beats Scissors!";
     }
-    else if (playerSelection == "paper" && computerSelection == "rock") {
+    else if (playerSelection === "paper" && computerSelection === "rock") {
         return "You win! Paper beats Rock!";
     }
-    else if (playerSelection == "scissors" && computerSelection == "paper") {
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
         return "You win! Scissors beats Paper!";
     }
     // Above segment is when players wins.
 
-    else if (playerSelection == "scissors" && computerSelection == "rock") {
+    else if (playerSelection === "scissors" && computerSelection === "rock") {
         return "You lose! Rock beats Scissors!";
     }
-    else if (playerSelection == "rock" && computerSelection == "paper") {
+    else if (playerSelection === "rock" && computerSelection === "paper") {
         return "You lose! Paper beats Rock!";
     }
-    else if (playerSelection == "paper" && computerSelection == "scissors") {
+    else if (playerSelection === "paper" && computerSelection === "scissors") {
         return "You lose! Scissors beats Paper!";
     }
     // Above segment is when computer wins.
 
+    else if (!(playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors")) {
+        return "You need to choose rock, paper or scissors!"
+    }
+
     else {
-        return "Draw!"
+        return "It's a Draw!";
     }
     //Draw
 }
+
+function game() {
+    let playerWins = 0;
+    let computerWins = 0;
+    
+    for (let i = 0; i < 5; i++) {
+        let roundPlayed = playRound(prompt("Do you choose Rock, Paper or Scissors?"), getComputerChoice())
+        console.log(roundPlayed)
+        if (roundPlayed.includes("win")) {playerWins++}
+        else if (roundPlayed.includes("lose")) {computerWins++}
+        else {
+            console.log("Try again!")
+            i--
+        }
+        console.log(`Player: ${playerWins}, Computer: ${computerWins}`)
+    }
+    if (playerWins > computerWins) {
+        console.log("Player wins! Well done!")
+    }
+    else {
+        console.log("Computer wins! Better luck next time!")
+    }
+}
+
+console.log("Best of 5 game! Good luck!")
+game()
