b61abf3ded51abe92466b1546c1fb90fd2e42f8f
Ideal <ideal.buzoku.21@gmail.com>
2022-11-17 04:13:42 +0000

    Write function that plays the game.

--- main.js
+++ main.js
@@ -1,12 +1,44 @@
 function getComputerChoice() {
-    randomNumber = Math.floor(Math.random() * 3)
+    randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber == 0) {
-        return "Rock"
+        return "Rock";
     }
     else if (randomNumber == 1) {
-        return "Paper"
+        return "Paper";
     }
     else {
-        return "Scissors"
+        return "Scissors";
     }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection.toLowerCase();
+    computerSelection.toLowerCase();
+     
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You win! Rock beats Scissors!";
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You win! Paper beats Rock!";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You win! Scissors beats Paper!";
+    }
+    // Above segment is when players wins.
+
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You lose! Rock beats Scissors!";
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You lose! Paper beats Rock!";
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You lose! Scissors beats Paper!";
+    }
+    // Above segment is when computer wins.
+
+    else {
+        return "Draw!"
+    }
+    //Draw
 }
